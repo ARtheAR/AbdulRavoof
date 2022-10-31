0d0ae4dfcabf875815df831f00f0a50cdece62c3
ARtheAR <101010992+ARtheAR@users.noreply.github.com>
2022-10-31 12:11:34 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,3 +10,26 @@ setTimeout("hide_canvas_inst()",5000);
 
 document.oncontextmenu =new Function("return false;");
 
+//scroll effect (opacity)
+
+function hide_element(i) {
+    window.addEventListener("scroll",()=>{
+        let content = i
+        let current_position = content.getBoundingClientRect().top;
+        let screen_position = window.innerHeight;
+        if (current_position < screen_position) {
+            content.classList.add("active");
+        }
+        else {
+            content.classList.remove("active");
+        }
+    });  
+}
+
+let headings_sub = document.querySelector(".headings-sub");
+let headings = document.querySelector(".headings");
+let timeline_subt = document.querySelector(".timeline_subt");
+
+hide_element(headings_sub);
+hide_element(headings);
+hide_element(timeline_subt);
